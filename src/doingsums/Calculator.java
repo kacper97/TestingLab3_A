9b7ca6137e1ec164e8c432c91ac7c8684978ea6b
Kacper Woloszyn <kacperwoloszyn4@gmail.com>
2017-09-29 00:33:01 +0100

    commit

---
+++ src/doingsums/Calculator.java
@@ -0,0 +1,12 @@
+
+package doingsums;
+
+public class Calculator {
+	public int add(int x, int y){
+		return x + y;
+	}
+	
+	public int sub(int x, int y){
+		return x - y;
+	}
+}
---
+++ tests/doingsums/CalculatorTests.java
@@ -0,0 +1,5 @@
+package doingsums;
+
+public class CalculatorTests {
+
+}
